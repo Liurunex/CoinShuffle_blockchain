d79168062ddd38c0c2b3443560b8b6f1f9b14b6a
Liurunex <liurunex@gmail.com>
2018-04-19 13:54:55 -0400

    blockchain start

---
+++ blockchain.py
@@ -0,0 +1,39 @@
+# Blockchian to managing the chain, storing transactions, adding new blocks
+'''
+block = {
+    'index': ...
+    'timestamp': ...
+    'proof': ...
+    'previous_hash': ...
+    'transactions': [
+        {
+            'sender': ...
+            'recipient': ...
+            'amount': ...
+        }
+    ],
+}
+'''
+
+class Blockchain(object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_block(self):
+        # Creates a new Block and adds it to the chain
+        pass
+
+    def new_transaction(self):
+        # Adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # Hashes a Block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last Block in the chain
+        pass
