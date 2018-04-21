f324d76d9af35d3e05242845a2203a74460363ea
Liurunex <liurunex@gmail.com>
2018-04-21 01:37:26 -0400

    block and transaction func added

--- blockchain.py
+++ blockchain.py
@@ -15,25 +15,52 @@ block = {
 }
 '''
 
+
+from time import time
+import json
+import  hashlib
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
+        # genesis block
+        self.new_block(previous_hash=1, proof=100)
 
-    def new_block(self):
+    def new_block(self, previous_hash, proof):
         # Creates a new Block and adds it to the chain
-        pass
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1]),
+        }
+        # reset current list of transaction
+        self.current_transactions = []
 
-    def new_transaction(self):
+        self.chain.append(block)
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
         # Adds a new transaction to the list of transactions
-        pass
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+        # return index of the block which the transaction will be added to
+        return self.last_block['index']+1
 
+    # decorator
     @staticmethod
     def hash(block):
-        # Hashes a Block
-        pass
+        # Hashes a Block with SHA-256 hash
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # Returns the last Block in the chain
-        pass
+        return self.chain[-1]
